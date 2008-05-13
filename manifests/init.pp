3dc005b552afd49de6efac96dbe024a2363fddf6
Marcel Haerry <haerry@puzzle.ch>
2008-05-13 14:55:56 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# nscd module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "nscd": }
+class nscd {}
